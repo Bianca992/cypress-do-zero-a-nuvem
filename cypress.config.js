fea64b29363700343da2a2ca7aadb74a5bcb519d
Bianca Martins <bmartins@gvdasa.com.br>
2025-09-17 11:16:46 -0300

    configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectid:'4toxcs'
+  projectid:'4toxcs',
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
